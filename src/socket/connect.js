74ac8a986e706ec628e5273db4cfdcd9d8f7801a
Pear TuanN <letuannhat105@gmail.com>
2025-01-17 14:37:03 +0700

    change enpoint

--- src/socket/connect.js
+++ src/socket/connect.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
-// const ENDPOINT = import.meta.env.VITE_BASE_URL_SERVER;
-const ENDPOINT = 'http://localhost:5000';
+const ENDPOINT = import.meta.env.VITE_BASE_URL_SERVER;
+// const ENDPOINT = 'http://localhost:5000';
 
 console.log(import.meta.env.VITE_BASE_URL_SERVER)
 let socket;
